9fa16a297be551c63ddd389d54ce6cd6bdf992e3
DOP <anders.bengtson@vd.ch>
2014-11-25 14:48:04 +0100

    fond de carte

--- controllers/carte.js
+++ controllers/carte.js
@@ -4,7 +4,7 @@ var connectString = 'tcp://' + bd.username + ':' + bd.password + '@localhost/pos
 
 exports.batOfEtabl = function(req, res) {
 	var idEtabl = req.params.idEtabl;
-	var query = 'SELECT id_bat_dgeo, bat.id_bat, bat.nom_bat, lat, lng FROM bat, bat_dgeo WHERE bat.id_bat=bat_dgeo.id_bat AND util=1 AND id_etabl=$1 ORDER BY nom_bat';
+	var query = 'SELECT id_bat_dgeo, bat.id_bat, bat.nom_bat, lat, lng FROM bat, bat_dgeo WHERE bat.id_bat=bat_dgeo.id_bat AND util=1 AND id_etabl=$1 AND lng IS NOT NULL ORDER BY nom_bat';
 	pg.connect(connectString, function(err, client, done) {
 		if(err) { return console.error('Problème de connection à la base de données', err); }
 		client.query(query, [idEtabl], function(err, result) {
